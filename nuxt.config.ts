1e4055a746fbc2f6198e04f2159da65839ca3ccf
WeiJia <2112025700@qq.com>
2023-08-19 14:02:59 +0800

    配置提高开发效率的插件和config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,18 @@
+import Components from "unplugin-vue-components/vite";
+import { AntDesignVueResolver } from "unplugin-vue-components/resolvers";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+  devtools: { enabled: true },
+  // 响应式 ref 省略 .value 处理数据，简化代码
+  experimental: {
+    reactivityTransform: true,
+  },
+  vite: {
+    plugins: [
+      Components({
+        resolvers: [AntDesignVueResolver()],
+      }),
+    ],
+  },
+});
---
+++ pages/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <a-button type="primary">我是按钮</a-button>
+</template>
+
+<script setup></script>
+
+<style scoped></style>
