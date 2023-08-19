931994d20aab4ac0a5ab9150e642530d07261ac2
WeiJia <2112025700@qq.com>
2023-08-19 14:24:20 +0800

    在项目中使用unocss

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,6 @@
 <template>
-  <a-button type="primary">我是按钮</a-button>
+  <div color-red bg-blue font-600 fs-30 w-100 h-100 flexc>测试 unocss</div>
+  <a-button color-green! bg-red!>按钮</a-button>
 </template>
 
 <script setup></script>
